1386b80ad5eceef80755652dd697fc723277f136
Vickie Moore <vickie.moore26@gmail.com>
2024-01-25 21:56:54 -0600

    Added example TDD test

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,8 +2,12 @@
 Calculator  functions
 """
 
+
 def add(x, y):
-   """
-   Add x and y to return a number
-   """
+   """Add x and y and return results."""
    return x + y
+
+
+def subtract(x, y):
+   """ Subtract x from y and return results."""
+   return y - x
